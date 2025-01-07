0c4136d2f307acddc93db87b956a89e20f413aa5
d1solis <d1solis@ucsd.edu>
2025-01-06 17:36:15 -0800

    Navbar & home WIP, standalones added to each component, enabled routing for navbar

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pagenotfound',
+  standalone: true,
   imports: [],
   templateUrl: './pagenotfound.component.html',
   styleUrl: './pagenotfound.component.css'
