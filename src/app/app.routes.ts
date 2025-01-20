2933806be12fd8f3d52c418c7dc748e54096a714
d1solis <d1solis@ucsd.edu>
2025-01-19 23:51:57 -0800

    Routing fix for blog page, minor fixes, WIP blog page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
     {'path': 'home', 'title' : 'Home', component:HomeComponent},
     {'path': 'about', 'title': 'About', component:AboutComponent},
+    {'path': 'blog', 'title': 'Blog', component:BlogComponent},
     {'path': 'contact', 'title': 'Contact', component:ContactComponent},
     {'path': '', redirectTo: '/home', pathMatch: 'full'},
-    {'path': '**', component: PagenotfoundComponent},
-    {'path': 'blog', 'title': 'Blog', component:BlogComponent}
+    {'path': '**', component: PagenotfoundComponent}
 
 ];
